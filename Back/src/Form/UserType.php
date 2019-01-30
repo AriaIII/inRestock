b59d3a6af368441a4ce79ca9f7025777a7718683
Caroline <poul445@free.fr>
2019-01-30 12:04:44 +0100

    début ajout des contraintes sur userType

--- Back/src/Form/UserType.php
+++ Back/src/Form/UserType.php
@@ -6,24 +6,100 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Entity\Role;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('firstName')
-            ->add('lastName')
-            ->add('username')
-            ->add('adress')
-            ->add('postcode')
-            ->add('town')
-            ->add('phone')
-            ->add('email')
-            ->add('password')
-            ->add('photo')
-            ->add('role')
-            ->add('post')
+            ->add('firstName', TextType::class, [
+                'label' => 'Prénom :',
+                'attr' => [
+                    'class' => 'input is-rounded'
+                    ]
+            ])
+            ->add('lastName', TextType::class, [
+                'label' => 'Nom de famille :',
+                'attr' => [
+                    'class' => 'input is-rounded'
+                    ]
+            ])
+            ->add('username', TextType::class, [
+                'label' => 'pseudo :',
+                'attr' => [
+                    'class' => 'input is-rounded'
+                    ]
+            ])
+            ->add('adress', TextareaType::class, [
+                'label' => 'Adresse :',
+                'attr' => [
+                    'class' => 'textarea'
+                    ]
+            ])
+            ->add('postcode',  TextType::class, [
+                'label' => 'Code postal :',
+                'attr' => [
+                    'class' => 'input is-rounded'
+                    ]
+            ])
+            ->add('town', TextType::class, [
+                'label' => 'Ville :',
+                'attr' => [
+                    'class' => 'input is-rounded'
+                    ]
+            ])
+            ->add('phone', TextType::class, [
+                'label' => 'Téléphone :',
+                'attr' => [
+                    'class' => 'input is-rounded'
+                    ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email :',
+                'attr' => [
+                    'class' => 'input is-rounded'
+                    ]
+            ])
+            ->add('photo', FileType::class, [
+                'label' => 'Photo :',
+                'attr' => [
+                    'class' => 'file'
+                ]
+            ])
+            ->add('role', EntityType::class, [
+                'label' => 'Rôle :',
+                'class' => Role::class,
+                'choice_label' => 'name',
+                'multiple' => false,
+                'expanded' => true,
+            ])
+            ->add('password', RepeatedType::class, [
+                'empty_data' => '',
+                'required' => false,
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les champs du mot passe doivent correspondre.',
+                'options' => [
+                    'attr' => [
+                        'class' => 'input is-rounded'
+                    ]
+                ],
+                'first_options'  => [
+                    'label' => 'Mot de passe :',
+                ],
+                'second_options' => [
+                    'label' => 'Verification du mot de passe :'
+                ],
+            ])
         ;
     }
 
