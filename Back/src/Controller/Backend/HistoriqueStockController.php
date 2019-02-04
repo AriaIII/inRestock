fad2381b7b5cb0d064c9097ea257775072e28a9f
De Sousa <de_sousa_alexandre@hotmail.com>
2019-02-04 15:49:29 +0100

    ajout de knp paginator et mise en place des filtres sur historiqueStock

--- Back/src/Controller/Backend/HistoriqueStockController.php
+++ Back/src/Controller/Backend/HistoriqueStockController.php
@@ -2,18 +2,36 @@
 
 namespace App\Controller\Backend;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\HistoriqueStockRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface;
 
 class HistoriqueStockController extends AbstractController
 {
     /**
      * @Route("backend/historique/stock", name="historique_stock")
      */
-    public function index(HistoriqueStockRepository $historiqueStockRepo)
+    public function index(HistoriqueStockRepository $historiqueStockRepo, EntityManagerInterface $em, Request $request, PaginatorInterface $paginator)
     {
-        $historiqueStock = $historiqueStockRepo->findAll();
+
+        // $historiqueStock = $historiqueStockRepo->findAll();
+        $dql   = "SELECT h FROM App:HistoriqueStock h
+                  JOIN  h.stock s
+                  JOIN s.product p
+                  JOIN h.user u
+                  JOIN h.modification m
+                ";
+        $query = $em->createQuery($dql);
+
+        $historiqueStock = $paginator->paginate(
+            $query, /* query NOT result */
+            $request->query->getInt('page', 1)/*page number*/,
+            5/*limit per page*/
+            );
+
 
         return $this->render('backend/historique_stock/index.html.twig', [
             'historiqueStock' => $historiqueStock,
