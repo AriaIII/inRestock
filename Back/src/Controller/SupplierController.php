b98a4ed8323f066d088fc297e043ad879640abf9
De Sousa <de_sousa_alexandre@hotmail.com>
2019-02-09 17:39:28 +0100

    index et show suppliers, inté et nav a faire

---
+++ Back/src/Controller/SupplierController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Supplier;
+use App\Repository\SupplierRepository;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class SupplierController extends AbstractController
+{
+    /**
+     * @Route("/fournisseurs", name="supplier_index")
+     */
+    public function index(SupplierRepository $supplierRepo)
+    {
+        $suppliers = $supplierRepo->findAll();
+
+        return $this->render('supplier/index.html.twig', [
+            "suppliers" => $suppliers     
+        ]);
+    }
+
+    /**
+     * @Route("/fournisseurs/{id}", name="supplier_show")
+     */
+    public function show(Supplier $supplier)
+    {
+        return $this->render('supplier/show.html.twig', [
+            "supplier" => $supplier     
+        ]);
+    }
+}
