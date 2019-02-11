be91c84dd12fa96d52c16ff6238a6c6a59d0578b
De Sousa <de_sousa_alexandre@hotmail.com>
2019-02-11 18:43:01 +0100

    debug mise en prod

--- Back/public/js/modification.js
+++ Back/public/js/modification.js
@@ -45,7 +45,7 @@ function onClickBtn(evt) {
     var user = valid.dataset.user; // id de l'utilisateur passé par twig
     var product =  valid.dataset.product; // id du produit 
    
-axios.post('/api/history/add', {
+axios.post('Back/public/api/history/add', {
     user: user,
     modification: modification,
     variation: input.value,
